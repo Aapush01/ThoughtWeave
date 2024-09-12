1ea63d18890146d0610e6ee4698f2704ce3e35ae
Aapush01 <mdshahidafridia31@gmail.com>
2024-09-12 19:59:37 +0530

    [routes]: blog routes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,1 +1,57 @@
- 
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono';
+import { verify } from 'hono/jwt'
+
+export const blogRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string,
+        JWT_SECRET: string
+    },
+    Variables: {
+        userId: string;
+    }
+}>();
+
+blogRouter.use("/*", async (c, next) => {
+    const authHeader = c.req.header("authorization") || "";
+    try {
+        const user = await verify(authHeader, c.env.JWT_SECRET);
+        if (user) {
+            c.set("userId", user.id);
+            await next();
+        } else {
+            c.status(403);
+            return c.json({
+                message: "You are not logged in"
+            })
+        }
+    } catch(e) {
+        c.status(403);
+        return c.json({
+            message: "You are not logged in"
+        })
+    }
+});
+
+blogRouter.post('/', async (c) => {
+    const body = await c.req.json();
+    const authorId = c.get('userId');
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate())
+  
+    const blog = await prisma.blog.create({
+        data: {
+            title: body.title,
+            content: body.content,
+            authorId: Number(authorId)
+        }
+    })
+
+    return c.json({
+        id: blog.id
+    })
+
+})
+
